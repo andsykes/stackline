4df588fce8b6f84c122f62148e5393e4250fc00e
Andrew Sykes <andrew.jon.sykes@gmail.com>
2024-05-27 18:24:05 -0700

    Updates

--- src/api/DataProvider.tsx
+++
@@ -1,6 +0,0 @@
-import data from "../data/stackline_frontend_assessment_data_2021.json";
-import { ListProductSalesResponse } from "./types";
-
-export const fetchAPIData = (): ListProductSalesResponse => {
-  return { productData: data };
-};
--- src/components/ProductThumbnail.tsx
+++
@@ -1,9 +0,0 @@
-interface ProductThumbnailProps {
-  image: string;
-  altText?: string;
-}
-
-export const ProductThumbnail = (props: ProductThumbnailProps) => {
-  const { image, altText } = props;
-  return <img style={{ width: "100%" }} src={image} alt={altText} />;
-};
